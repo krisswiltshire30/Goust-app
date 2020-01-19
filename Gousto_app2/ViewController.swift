2ef3353a72c827df3482c85cbcd8288f8c30f395
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2020-01-19 13:04:12 +0000

    Initial Commit

---
+++ Gousto_app2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gousto_app2
+//
+//  Created by Kriss Wiltshire on 19/01/2020.
+//  Copyright © 2020 Kriss Wiltshire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
